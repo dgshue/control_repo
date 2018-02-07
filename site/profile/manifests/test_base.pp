42a6791793a1514462755104d9a89565db99672a
dgshue <daniel.shue@randolphcountync.gov>
2018-02-07 10:41:00 -0500

    Delete test_base.pp

--- site/profile/manifests/test_base.pp
+++
@@ -1,9 +0,0 @@
-class profile::test_base {
-  class { 'wsus_client':
-    server_url             => 'http://rcupdate01.co.randolph.nc.us:8530',
-    auto_update_option     => 'Scheduled',
-    scheduled_install_day  => 'Tuesday',
-    scheduled_install_hour => 2,
-  }
-  contain 'wsus_client'
-}
