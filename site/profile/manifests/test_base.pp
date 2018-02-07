f5716d4dd021f6a13e096a1aa03dd022f6565ccf
dgshue <daniel.shue@randolphcountync.gov>
2018-02-07 10:57:13 -0500

    Merge branch 'production' of https://github.com/dgshue/control_repo into production

---
+++ site/profile/manifests/test_base.pp
@@ -0,0 +1,9 @@
+class profile::test_base {
+  class { 'wsus_client':
+    server_url             => 'http://rcupdate01.co.randolph.nc.us:8530',
+    auto_update_option     => 'Schedule',
+    scheduled_install_day  => 'Tuesday',
+    scheduled_install_hour => 2,
+  }
+  contain 'wsus_client'
+}
