5524bcd46b039bc8757942f886fbff54ea3ff01e
dgshue <daniel.shue@randolphcountync.gov>
2018-02-12 20:14:24 -0500

    Fix1

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -5,23 +5,15 @@ class profile::base {
   elsif $facts['os']['family'] == 'RedHat' {
     warning('WSUS not needed')
     package { 'htop':
-<<<<<<< HEAD
-      ensure => 'installed',
+      ensure => 'present',
       name   => 'htop'
-=======
-    ensure => 'installed',
->>>>>>> 548ed91d3a75704a522f9615a784fccf296cbdb9
     }
   }
   elsif $facts['osfamily'] == 'Debian' {
     warning('WSUS not needed')
     package { 'htop':
-<<<<<<< HEAD
-      ensure => 'installed',
+      ensure => 'present',
       name   => 'htop'
-=======
-    ensure => 'installed',
->>>>>>> 548ed91d3a75704a522f9615a784fccf296cbdb9
     }
   }
   else {
