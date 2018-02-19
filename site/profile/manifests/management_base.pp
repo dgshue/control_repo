a37c7b4769f17d9e15ea9b0dbc840d397648b04a
dgshue <daniel.shue@randolphcountync.gov>
2018-02-19 16:12:09 -0500

    fix wsus

--- site/profile/manifests/management_base.pp
+++ site/profile/manifests/management_base.pp
@@ -1,11 +1,5 @@
 class profile::management_base {
-  class { 'wsus_client':
-    server_url             => 'http://rcupdate01.co.randolph.nc.us:8530',
-    auto_update_option     => 'Scheduled',
-    scheduled_install_day  => 'Tuesday',
-    scheduled_install_hour => 2,
-  }
-  contain 'wsus_client'
+  include test_wsus
   service { 'puppet':
       ensure => 'running',
       enable => true,
