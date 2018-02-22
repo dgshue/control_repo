b8dec5361b5cdcbea9ad584829bb558fa3460946
dgshue <daniel.shue@randolphcountync.gov>
2018-02-22 16:42:05 -0500

    Final commit of this repo - moving to RandolphCountyNC

--- site/profile/manifests/dmz.pp
+++ site/profile/manifests/dmz.pp
@@ -4,6 +4,12 @@ class profile::dmz {
       ensure => 'stopped',
       enable => false,
     }
+    #clean up from install
+    file { 'C:/Zimptemp/Puppet':
+      ensure  => 'abenst',
+      path    => 'C:/Zimptemp/Puppet',
+      recurse => true,
+    }
   }
   elsif $facts['os']['family'] == 'RedHat' {
 
